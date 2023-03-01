8fac1b11aa37005d26b339777062fdf5cf5413d2
white <3179167073@qq.com>
2023-03-01 19:12:58 +0800

    :sparkles:笔记模块搭建

---
+++ waveedu-note/src/main/java/com/zhulang/waveedu/note/WaveeduNoteApplication.java
@@ -0,0 +1,15 @@
+package com.zhulang.waveedu.note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 狐狸半面添
+ * @create 2023-03-01 19:08
+ */
+@SpringBootApplication
+public class WaveeduNoteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WaveeduNoteApplication.class);
+    }
+}
