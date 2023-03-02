18d6360bd39a9121a838702dd6d03c7e0260ecda
white <3179167073@qq.com>
2023-03-03 03:02:47 +0800

    :sparkles:聊天模块搭建

---
+++ waveedu-chat/src/main/java/com/zhulang/waveedu/chat/WaveeduChatApplication.java
@@ -0,0 +1,15 @@
+package com.zhulang.waveedu.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 狐狸半面添
+ * @create 2023-03-01 19:08
+ */
+@SpringBootApplication
+public class WaveeduChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WaveeduChatApplication.class);
+    }
+}
