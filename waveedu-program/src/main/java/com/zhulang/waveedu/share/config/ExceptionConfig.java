1a69356fbe18c77b18b542b26b824fac00a0d02d
white <3179167073@qq.com>
2023-03-11 15:33:24 +0800

    :sparkles:搭建编程模块

---
+++ waveedu-program/src/main/java/com/zhulang/waveedu/share/config/ExceptionConfig.java
@@ -0,0 +1,17 @@
+package com.zhulang.waveedu.share.config;
+
+import com.zhulang.waveedu.common.exception.WaveEduExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 狐狸半面添
+ * @create 2023-01-18 17:43
+ */
+@Configuration
+public class ExceptionConfig {
+    @Bean
+    public WaveEduExceptionHandler waveEduExceptionHandler(){
+        return new WaveEduExceptionHandler();
+    }
+}
