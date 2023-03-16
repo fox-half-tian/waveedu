d35f063207e69e9873214d2b75a762b6bee979b3
white <3179167073@qq.com>
2023-03-16 21:27:59 +0800

    :sparkles:搭建虚拟机模块

---
+++ waveedu-vm/src/main/java/com/zhulang/waveedu/vm/LinuxController.java
@@ -0,0 +1,13 @@
+package com.zhulang.waveedu.vm;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 狐狸半面添
+ * @create 2023-03-16 21:26
+ */
+@RestController
+@RequestMapping("/linux")
+public class LinuxController {
+}
